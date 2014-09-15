02c6f86b1ad292d1fe1bddcb151d3713d95ccd10
Ivan Orlov <ivanorlo@amazon.com>
2014-09-15 14:39:02 -0700

    [appearance] Use specific color for computer name

--- bash/040_appearance.bash
+++ bash/040_appearance.bash
@@ -6,7 +6,10 @@ __prompt_command() {
   local _R="\e[38;5;160m"
   local _G="\e[38;5;70m"
   local _B="\e[38;5;104m"
+  local _O="\e[38;5;208m"
   local _0="\e[0m"
+
+  [ -z $__NAME_COLOR ] && __NAME_COLOR=$_O
 
   local path=${1:-$(pwd)}
   local pkg=''
@@ -18,7 +21,7 @@ __prompt_command() {
       path="$_G$pkg$_0 $path"
     fi
   fi
-  export PS1="$_B $COMPUTER_NAME$_0 $path $_G$(__git_ps1)$_0
+  export PS1="$__NAME_COLOR $COMPUTER_NAME$_0 $path $_G$(__git_ps1)$_0
 \$ "
 }
 
